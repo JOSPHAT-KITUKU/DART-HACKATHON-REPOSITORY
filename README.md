243de3c5d81abe3f879e3bea086802239ed8f92a
Josphat Kituku <kitukujosphat@gmail.com>
2024-03-16 23:17:53 +0300

    Control flow class displaying grade and student information

--- README.md
+++ README.md
@@ -22,3 +22,6 @@ Write a Dart program to determine the grade based on a student's marks. The prog
 - If the marks are between 75 and 85 (inclusive), print "Very Good".
 - If the marks are between 65 and 75 (inclusive), print "Good".
 - If the marks are below 65, print "Average".
+
+# Solution
+I have solved by creating a student class to process the student information. After that created a function to handle grade. Then now using an instance of the class printed out the information and graded mark.
--- controlflow.dart
+++ controlflow.dart
@@ -1,7 +1,52 @@
 
-## Program 3: Determine Grade Based on Marks
-Write a Dart program to determine the grade based on a student's marks. The program should print out the appropriate grade according to the following criteria:
-- If the marks are greater than 85, print "Excellent".
-- If the marks are between 75 and 85 (inclusive), print "Very Good".
-- If the marks are between 65 and 75 (inclusive), print "Good".
-- If the marks are below 65, print "Average".
+//## Program 3: Determine Grade Based on Marks
+//Write a Dart program to determine the grade based on a student's marks. The program should print out the appropriate grade according to the following criteria:
+//- If the marks are greater than 85, print "Excellent".
+//- If the marks are between 75 and 85 (inclusive), print "Very Good".
+//- If the marks are between 65 and 75 (inclusive), print "Good".
+//- If the marks are below 65, print "Average".
+
+import 'dart:io';
+
+class Student{
+    String name = '';
+    String adm_no = '';
+    String stud_class1 = '';
+    String graded = '';
+
+    //create an instance of the class
+    Student(this.adm_no, this.name, this.stud_class1, this.graded);
+
+    void displayInfor(){
+        print("ADM NO || STUD NAME|| CLASS || GRADE");
+        print("$adm_no || $name || $stud_class1 || $graded" );
+    }
+}
+String grade(score){
+    if (score > 85){
+        return "Excellent";
+    }else if (score > 75){
+        return "Very Good";
+    }else if (score > 65){
+        return "Good";
+    }else{
+        return "Average";
+    }
+}
+void main(){
+    print("Enter Student Name:");
+    String? stud_name = stdin.readLineSync();
+    print("Enter Student Admission Number:");
+    String? stud_adm = stdin.readLineSync();
+    print("Enter Student Class:");
+    String? stud_class = stdin.readLineSync();
+    print("Enter Student Score:");
+    int? marks = int.parse(stdin.readLineSync()!);
+
+    String grades = grade(marks);
+    print("$grades");
+
+    var student = Student('$stud_adm', '$stud_name', '$stud_class', '$grades');
+
+    student.displayInfor();
+}
