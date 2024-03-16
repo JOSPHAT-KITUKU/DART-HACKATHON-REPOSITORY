b39f0f177bef93f6d882a889a4d7939fe13fac53
Josphat Kituku <kitukujosphat@gmail.com>
2024-03-16 21:52:15 +0300

    Using functions to add and subtract values

--- README.md
+++ README.md
@@ -7,11 +7,14 @@
 ## Program 1: Display Personal Information
 Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
 
-## Solution
+# Solution
 I have solved the problem by declaring the information variables then printing them out.
 
 ## Program 2: Perform Mathematical Operations with Functions
 Write a Dart program that performs two mathematical operations using functions.
+
+# Solution
+I have solved by introducing two functions one name sum and another named subtraction. Sum takes in values gotten as prameters and performs the addition while subtraction subtracts.
 
 ## Program 3: Determine Grade Based on Marks
 Write a Dart program to determine the grade based on a student's marks. The program should print out the appropriate grade according to the following criteria:
--- functions.dart
+++ functions.dart
@@ -1,2 +1,36 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+//## Program 2: Perform Mathematical Operations with Functions
+//Write a Dart program that performs two mathematical operations using functions.
+
+import 'dart:io';
+
+int sum(num1, num2){
+    //declaration of the variable result.
+    int result = 0;
+    //perform the addition
+    result = num1 + num2;
+    return result;
+}
+int substrction(num1, num2){
+    int result = 0;
+
+    result = num1 - num2;
+
+    return result;
+}
+
+void main(){
+    //taking in user input
+    print("Enter Num1");
+    int? number1 = int.parse(stdin.readLineSync()!);
+    print("Enter Num2");
+    int? number2 = int.parse(stdin.readLineSync()!);
+
+    //doing the calculations using the functions
+    int add_result = sum(number1, number2);
+    int sub_result = substrction(number1, number2);
+
+    //displaying the output on the stdout
+    print("Sum of the numbers is: $add_result");
+
+    print("Subtraction of the numbers is: $sub_result");
+}
